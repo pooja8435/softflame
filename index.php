a755f4b27038ef15ff519599ebb5a55eb42002bd
megha-softflame <d100180.html@softflameserver.com>
2023-11-07 11:19:49 +0530

    minor chng on home page

--- index.php
+++ index.php
@@ -338,13 +338,13 @@
 }
 
 .card h2:after {
-  background: linear-gradient(to right, #fff, #007bff);
+  background: linear-gradient(to right, #a2cfe9, #007bff);
   right: -5px;
   top: -5px;
 }
 
 .card h2:before {
-  background: linear-gradient(to right, #007bff, #fff);
+  background: linear-gradient(to right, #007bff, #a2cfe9);
   left: -5px;
   bottom: -5px;
 }
@@ -667,6 +667,10 @@ header:hover .owl-prev {
 header:hover .owl-next {
   right: 0px;
   opacity: 1;
+}
+
+a:hover{
+    color:#305e91;
 }
 
 
@@ -1061,6 +1065,7 @@ header:hover .owl-next {
     
     </div>
     <div class="card">
+    <a href="android-app-development-company-in-pune.php">
       <div class="icon">
        <!-- <i class="fab fa-android"></i> -->
        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"/></svg>
@@ -1068,8 +1073,10 @@ header:hover .owl-next {
       <h2>App Development</h2>
       <p>We, at SoftFlame Solutions expertise in Mobile App Development Company in Pune,
                                 which allow all range of App development solution.</p>
+    </a>
       </div>
       <div class="card">
+      <a href="seo-company-in-pune.php">
         <div class="icon">
           <!-- <i class="fas fa-globe-europe"></i> -->
           <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 460 512"><path d="M220.6 130.3l-67.2 28.2V43.2L98.7 233.5l54.7-24.2v130.3l67.2-209.3zm-83.2-96.7l-1.3 4.7-15.2 52.9C80.6 106.7 52 145.8 52 191.5c0 52.3 34.3 95.9 83.4 105.5v53.6C57.5 340.1 0 272.4 0 191.6c0-80.5 59.8-147.2 137.4-158zm311.4 447.2c-11.2 11.2-23.1 12.3-28.6 10.5-5.4-1.8-27.1-19.9-60.4-44.4-33.3-24.6-33.6-35.7-43-56.7-9.4-20.9-30.4-42.6-57.5-52.4l-9.7-14.7c-24.7 16.9-53 26.9-81.3 28.7l2.1-6.6 15.9-49.5c46.5-11.9 80.9-54 80.9-104.2 0-54.5-38.4-102.1-96-107.1V32.3C254.4 37.4 320 106.8 320 191.6c0 33.6-11.2 64.7-29 90.4l14.6 9.6c9.8 27.1 31.5 48 52.4 57.4s32.2 9.7 56.8 43c24.6 33.2 42.7 54.9 44.5 60.3s.7 17.3-10.5 28.5zm-9.9-17.9c0-4.4-3.6-8-8-8s-8 3.6-8 8 3.6 8 8 8 8-3.6 8-8z"/></svg>
@@ -1077,8 +1084,10 @@ header:hover .owl-next {
         <h2>SEO Company</h2>
         <p>SEO Company in Pune, Bangalore, Delhi is the way for any business to garner
                                 higher volumes of qualified traffic and web popularity...</p>
+    </a>
     </div>
     <div class="card">
+    <a href="ecommerce-development-company-in-pune.php">
         <div class="icon">
           <!-- <i class="fas fa-globe-europe"></i> -->
           <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><style>svg{fill:#305e91;width:60%;}</style><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>
@@ -1086,6 +1095,7 @@ header:hover .owl-next {
         <h2>Ecommerce</h2>
         <p>SoftFlame Solutions brings unique features in Ecommerce Web Development Company
                                 in Pune, Bangalore, Delhi from startups to established companies.</p>
+    </a>
     </div>
   </div>
 </div>
